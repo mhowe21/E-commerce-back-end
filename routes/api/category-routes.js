99780483f2fde6f0e57df576278ed8af1f9a7a04
Mason Howe <mason.a.howe@gmail.com>
2021-02-13 14:17:27 -0700

    init commit

---
+++ routes/api/category-routes.js
@@ -0,0 +1,70 @@
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
+
+// The `/api/categories` endpoint
+
+router.get("/", (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  Category.findAll({
+    include: [Product],
+  }).then((data) => {
+    res.status(200).json(data);
+  });
+});
+
+router.get("/:id", (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  Category.findAll({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(404).json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  // create a new category
+  Category.create(req.body).then((data) => {
+    res.status(201).json(data);
+  });
+});
+
+router.put("/:id", (req, res) => {
+  // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(404).json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(404).json(err);
+    });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const routes = require("./routes");
+// import sequelize connection
+const sequilize = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(require("./routes/index"));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+sequilize.sync({ force: true }).then(function () {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+});
